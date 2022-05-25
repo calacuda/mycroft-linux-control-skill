07ba273af4d97cf7ea30fad62b0d94414e6d6a62
yogurt <eowest@gmail.com>
2022-05-25 15:59:46 -0400

    testing

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ from subprocess import run, PIPE
 import sockets
 from os import listdir
 from os.path import isfile, expanduser, basename
+from pathlib import Path
 
 
 
@@ -115,7 +116,7 @@ class LinuxControl(MycroftSkill):
         for f in listdir(expanduser("~/.config/desktop-automater/layouts/")):
             f_basename = ".".join(basename(f).split(".")[:-1])
             self.log.warning(f"looking at file {f_basename}")
-            if f.is_file() and f_basename.lower() == processed_layout:
+            if Path(f).is_file() and f_basename.lower() == processed_layout:
                 layout = layout.replace(" ", "-")
                 self.speak_dialog(f"configuring layout {layout}")
                 return self.api_send(f"load-layout {f_basename}", "layout-load-success", "layout-load-failed")
